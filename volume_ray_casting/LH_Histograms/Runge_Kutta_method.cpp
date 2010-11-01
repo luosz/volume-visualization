1f628bb35fa08d30848274bde856ddc41fda79d7
LovRNA <LovRNA@localhost>
2010-11-01 17:25:28 +0800

    backup

---
+++ volume_ray_casting/LH_Histograms/Runge_Kutta_method.cpp
@@ -0,0 +1,19 @@
+typedef struct {
+	int x;
+	int y;
+	int z;
+}point;
+
+typedef struct
+{
+	unsigned int x;
+	unsigned int y;
+	unsigned int z;
+}grid;
+
+
+void second_order_Runge_Kutta(point &initialPoint, int integrationStep, grid &initialGradients){
+
+	
+};
+
