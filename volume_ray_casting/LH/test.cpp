5cb518577d440f65d116ef38ec40a515d890e3f1
LovRNA <LovRNA@localhost>
2010-11-07 17:59:00 +0800

    fix bugs in the generic types

---
+++ volume_ray_casting/LH/test.cpp
@@ -0,0 +1,166 @@
+#include "../my_raycasting/VolumeReader.h" 
+#include "LH_Histograms_Constructor.h"
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <nvMath.h>
+using namespace std;
+
+//void generate_gradient(const unsigned int datalength,const unsigned int datawidth,const unsigned int dataheight, unsigned int *scalar_value, std::vector<nv::vec3f> &gradient, std::vector<float> &gradient_magnitude, float &max_gradient_magnitude, std::vector<nv::vec3f> &second_derivative, std::vector<float> &second_derivative_magnitude, float &max_second_derivative_magnitude)
+//{
+//
+//	//std::cout<<"gradient vector address = "<<&gradient<<std::endl;
+//	unsigned int index;
+//	int boundary[3] = {datalength-1, datawidth-1, dataheight-1};
+//	unsigned int width = datalength, height = datawidth, depth = dataheight;
+//
+//	max_second_derivative_magnitude = max_gradient_magnitude = -1;
+//	for (int i=0; i</*datalength*/depth; i++)
+//	{
+//		for (int j=0; j</*datawidth*/height; j++)
+//		{
+//			for (int k=0; k</*dataheight*/width; k++)
+//			{
+//				index = ((i) * height + j) * width + k;
+//				//index = k*datalength*datawidth + j*datalength +i;
+//				//std::cout<<"index = "<<index<<std::endl;
+//				if (k==0 || j==0 || i==0 || k==boundary[0] || j==boundary[1] || i==boundary[2])
+//				{
+//					gradient_magnitude[index] = gradient[index].x = gradient[index].y = gradient[index].z = 0;
+//				}else
+//				{
+//					gradient[index].x = scalar_value[((i + 1) * height + j) * width + k] - scalar_value[((i - 1) * height + j) * width + k];
+//					gradient[index].y = scalar_value[((i) * height + j + 1) * width + k] - scalar_value[((i) * height + j - 1) * width + k];
+//					gradient[index].z = scalar_value[((i) * height + j) * width + k + 1] - scalar_value[((i) * height + j) * width + k - 1];
+//					gradient_magnitude[index] = nv::length(gradient[index]);
+//					max_gradient_magnitude = std::max(gradient_magnitude[index], max_gradient_magnitude);
+//				}
+//			}
+//		}
+//	}
+//
+//	for (int i=0; i<depth; i++)
+//	{
+//		for (int j=0; j<height; j++)
+//		{
+//			for (int k=0; k<width; k++)
+//			{
+//				index = ((i) * height + j) * width + k;
+//				if (k==0 || j==0 || i==0 || k==boundary[0] || j==boundary[1] || i==boundary[2])
+//				{
+//					second_derivative_magnitude[index] = 0;
+//				}else
+//				{
+//					second_derivative[index].x = gradient[((i + 1) * height + j) * width + k].x - gradient[((i - 1) * height + j) * width + k].x;
+//					second_derivative[index].y = gradient[((i) * height + j + 1) * width + k].y - gradient[((i) * height + j - 1) * width + k].y;
+//					second_derivative[index].z = gradient[((i) * height + j) * width + k + 1].z - gradient[((i) * height + j) * width + k - 1].z;
+//					second_derivative_magnitude[index] = nv::length(second_derivative[index]);
+//					max_second_derivative_magnitude = std::max(second_derivative_magnitude[index], max_second_derivative_magnitude);
+//				}
+//			}
+//		}
+//	}
+//}
+
+void generate_gradient(VolumeReader &vv,const unsigned int datalength,const unsigned int datawidth,const unsigned int dataheight, std::vector<nv::vec3f> &gradient, std::vector<float> &gradient_magnitude, float &max_gradient_magnitude, std::vector<nv::vec3f> &second_derivative, std::vector<float> &second_derivative_magnitude, float &max_second_derivative_magnitude)
+{
+	//std::cout<<"gradient vector address = "<<&gradient<<std::endl;
+	unsigned int index;
+	int boundary[3] = {datalength-1, datawidth-1, dataheight-1};
+	//unsigned int width = datalength, height = datawidth, depth = dataheight;
+
+	max_second_derivative_magnitude = max_gradient_magnitude = -1;
+	for (int i=0; i<datalength/*depth*/; i++)
+	{
+		for (int j=0; j<datawidth/*height*/; j++)
+		{
+			for (int k=0; k<dataheight/*width*/; k++)
+			{
+				//index = ((i) * height + j) * width + k;
+				index = vv.getIndex(i,j,k);
+				//std::cout<<"index = "<<index<<std::endl;
+				if (k==0 || j==0 || i==0 || k==boundary[0] || j==boundary[1] || i==boundary[2])
+				{
+					gradient_magnitude[index] = gradient[index].x = gradient[index].y = gradient[index].z = 0;
+				}else
+				{
+					
+						gradient[index].x = (float)vv.getData(i+1,j,k) - vv.getData(i-1,j,k);
+						gradient[index].y = (float)vv.getData(i,j+1,k) - vv.getData(i,j-1,k);
+						gradient[index].z = (float)vv.getData(i,j,k+1) - vv.getData(i,j,k-1);
+						gradient_magnitude[index] = nv::length(gradient[index]);
+						max_gradient_magnitude = std::max(gradient_magnitude[index], max_gradient_magnitude);
+				
+				}
+			}
+		}
+	}
+
+	for (int i=0; i<datalength/*depth*/; i++)
+	{
+		for (int j=0; j<datawidth/*height*/; j++)
+		{
+			for (int k=0; k<dataheight/*width*/; k++)
+			{
+				//index = ((i) * height + j) * width + k;
+				index = vv.getIndex(i,j,k);
+				if (k==0 || j==0 || i==0 || k==boundary[0] || j==boundary[1] || i==boundary[2])
+				{
+					second_derivative_magnitude[index] = 0;
+				}else
+				{
+					second_derivative[index].x = gradient[(k*datawidth + j)*datalength +i+1].x - gradient[(k*datawidth + j)*datalength +i-1].x;
+					second_derivative[index].y = gradient[(k*datawidth + (j+1))*datalength +i].y - gradient[(k*datawidth + (j-1))*datalength +i].y;
+					second_derivative[index].z = gradient[((k+1)*datawidth + j)*datalength +i].z - gradient[((k-1)*datawidth + j)*datalength +i].z;
+					second_derivative_magnitude[index] = nv::length(second_derivative[index]);
+					max_second_derivative_magnitude = std::max(second_derivative_magnitude[index], max_second_derivative_magnitude);
+				}
+			}
+		}
+	}
+}
+
+void main()
+{
+
+	VolumeReader volume;
+	char *volume_filename = "../my_raycasting/data/nucleon.dat";
+	volume.readVolFile(volume_filename);
+	unsigned int count = volume.getCount();
+
+	std::vector<nv::vec3f> gradient(count);
+	std::vector<float> grandient_magnitude(count);
+	std::vector<nv::vec3f> second_derivative(count);
+	std::vector<float> second_derivative_magnitude(count);
+	float maxGradientMagnitude;
+	float maxSecondDerivativeMagnitude;
+	generate_gradient(volume,volume.getX(),volume.getY(),volume.getZ(),gradient,grandient_magnitude,maxGradientMagnitude,second_derivative,second_derivative_magnitude,maxSecondDerivativeMagnitude);
+
+
+	std::ofstream out("D:/result.txt");
+	for(int i = 0; i <= volume.getX()-1; i++){
+		for(int j = 0; j <= volume.getY()-1; j++ ){
+			for(int k = 0; k <= volume.getZ()-1; k++){
+				unsigned int index = volume.getIndex(i,j,k);
+				out<<"voxel coordinate = ("<< i <<","<<j<<","<<k<<");\t";
+				out<<"voxel Index = "<<index<<";\t";
+				out<<"voxel data = "<<volume.getData(i,j,k)<<";\t"<<std::endl;;
+				out<<"voxel gradient = ("<<gradient[index].x<<","<<gradient[index].y<<","<<gradient[index].z<<");\t";
+				out<<"voxel gradient magnitude = "<<grandient_magnitude[index]<<";\t";
+				out<<"voxel second derivative = ("<<second_derivative[index].x<<","<<second_derivative[index].y<<","<<second_derivative[index].z<<");\t";
+				out<<"voxel second derivative magnitude = "<<second_derivative_magnitude[index]<<";\t"<<std::endl;
+			}
+		}
+	}
+
+	LH_Histograms LHHistograms = LH_Histograms();
+	LHHistograms.constructor(volume,gradient,grandient_magnitude,second_derivative,second_derivative_magnitude);
+	out.close();
+	out.open("d:/FLandFH.txt");
+	for(std::vector<std::pair<nv::vec3f,std::pair<float,float>>>::iterator it = LHHistograms.m_LHInformation.begin();it !=LHHistograms.m_LHInformation.end(); it++){
+		out<<"coordinate = ("<<it->first.x<<","<<it->first.y<<","<<it->first.z<<")\t";
+		out<<"FL and FH values = ("<<it->second.first<<","<<it->second.second<<")"<<std::endl;
+	}
+	//std::cout<<"Hello"<<std::endl;
+}
---
+++ volume_ray_casting/my_raycasting/VolumeReader.h
@@ -0,0 +1,194 @@
+#ifndef VolumeReader_h
+#define VolumeReader_h
+
+#include <cstdio>
+#include <cctype>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+
+#include "../BenBenRaycasting/Volume.h"
+#include "../my_raycasting/reader.h"
+#include "../my_raycasting/filename_utility.h"
+
+struct AccessBase
+{
+	virtual unsigned int getData(unsigned int index) = 0;
+};
+
+template<class T>
+struct AccessGeneric : AccessBase
+{
+	T  datatransfered;
+
+	AccessGeneric(void * d)
+	{
+		datatransfered = (T)d;
+	}
+
+	virtual unsigned int getData(unsigned int index)
+	{
+		return (unsigned int)datatransfered[index];
+	}
+};
+
+struct AccessChar : AccessBase
+{
+	unsigned char * datatransfered;
+
+	AccessChar(void * d)
+	{
+		datatransfered = (unsigned char *)d;
+	}
+
+	virtual unsigned int getData(unsigned int index)
+	{
+		return (unsigned int)datatransfered[index];
+	}
+};
+
+class VolumeReader : public Volume
+{
+protected:
+	AccessBase * accessor;
+
+public:
+
+	VolumeReader(void)
+	{
+		accessor = NULL;
+	}
+
+	virtual ~VolumeReader(void)
+	{
+		if (accessor)
+		{
+			delete accessor;
+		}
+	}
+
+	// get the .raw file name from the .dat file automatically
+	virtual bool readVolFile(char * s)
+	{
+		char * cp, line[100], rawFilename[100];
+		FILE * fp = fopen(s, "r");
+
+		printf("Reading data description file %s ......\n", s);
+		if(fp == NULL)
+		{
+			fprintf(stderr, "ERROR: Could not open file %s\n", s);
+			return false;
+		}
+		while(fgets(line, sizeof(line), fp))
+		{
+			if(strstr(line, "ObjectFileName"))
+			{
+				cp = strchr(line, ':');
+				sscanf(cp + 1, "%s", rawFilename);
+				printf("Get data file name: %s ......\n", rawFilename);
+			}
+			else if(strstr(line, "Resolution"))
+			{
+				cp = strchr(line, ':');
+				sscanf(cp + 1, "%i %i %i", &length, &width, &height);
+				count = length * width * height;
+				printf("Get data's resolution\nlength = %i\nwidth = %i\nheight = %i\n", length, width, height);
+			}
+			else if(strstr(line, "Format"))
+			{
+				cp = strchr(line, ':');
+				if(strstr(line, "UCHAR"))
+				{
+					strcpy(format, "UCHAR");
+					printf("Get data's format: Unsigned Char\n");
+					dataTypeSize = sizeof(unsigned char);
+					range = 256;
+				}
+				else if(strstr(line, "USHORT"))
+				{
+					strcpy(format, "USHORT");
+					printf("Get data's format: Unsigned Short\n");
+					dataTypeSize = sizeof(unsigned short);
+					range = 65536;
+				}
+				else 
+					fprintf(stderr, "invalid data format\n");
+			}	
+			else
+				fprintf(stderr, "skipping line %s\n", line);
+		}
+		fclose(fp);
+
+		//////////////////////////////////////////////////////////////////////////
+		// get the raw file path and filename
+		//std::cout<<"VolumeReader::readVolFile - get the raw file path and filename"<<std::endl;
+		char str[MAX_STR_SIZE];
+		filename_utility::get_raw_filename_from_dat_filename(s, rawFilename, str);
+		Volume::readData(str);
+
+		if(strcmp(format, "UCHAR") == 0)
+		{
+			accessor = new AccessGeneric<unsigned char *>(data);
+		}else
+		{
+			if(strcmp(format, "USHORT") == 0)
+			{
+				accessor = new AccessGeneric<unsigned short *>(data);	
+			}else 
+			{
+				std::cerr<<"Unsupported data type in "<<s<<std::endl;
+			}
+		}
+		//////////////////////////////////////////////////////////////////////////
+		
+		return true;
+	}
+
+	virtual unsigned int getData(unsigned int x, unsigned int y, unsigned int z)
+	{
+		return accessor->getData(getIndex(x, y, z));
+	}
+
+	// read volume data from file using readData in reader.h
+	void readVolume_reader(char* filename)
+	{
+		using namespace reader;
+
+		void ** data_ptr = new void *;
+		int sizes[3];
+		int color_omponent_number;
+		float dists[3];
+		DataType type;
+		reader::readData(filename, sizes, dists, data_ptr, &type, &color_omponent_number);
+
+		// Please set the properties of class volume to protected, in order to set their values.
+		length = sizes[0];
+		width = sizes[1];
+		height = sizes[2];
+		count = length * width * height;
+
+		switch (type)
+		{
+		case DATRAW_UCHAR:
+			strcpy(format, "UCHAR");
+			dataTypeSize = sizeof(unsigned char);
+			range = 256;
+			accessor = new AccessGeneric<unsigned char *>(data);
+			break;
+		case DATRAW_USHORT:
+			strcpy(format, "USHORT");
+			dataTypeSize = sizeof(unsigned short);
+			range = 65536;
+			accessor = new AccessGeneric<unsigned short *>(data);
+			break;
+		//default:
+			//std::cerr<<"Unsupported data type in "<<filename<<std::endl;
+		}
+
+		data = *data_ptr;
+		delete data_ptr;
+		data_ptr = NULL;
+	}
+};
+
+#endif // VolumeReader_h
