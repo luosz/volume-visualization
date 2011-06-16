42fbc3f1506e5c933297538359e11606e6c9f76c
ark <ark@localhost>
2011-06-16 10:55:59 +0800

    format codes

--- volume_ray_casting/my_raycasting/reader.h
+++ volume_ray_casting/my_raycasting/reader.h
@@ -11,150 +11,188 @@
  * prohibited.
  *
  * Filename: reader.h
- * 
+ *
  */
 
 #ifndef _READER_H
 #define _READER_H
 
-namespace volume_utility{
-
+namespace volume_utility
+{
+	/// data type of volume data
 	typedef enum {DATRAW_UCHAR, DATRAW_FLOAT, DATRAW_USHORT} DataType;
 
 	/// read raw data from files
-	void readData(char *filename, int *sizes, float *dists, void **data, 
-		DataType *type, int *numComponents);
+	void readData(char *filename, int *sizes, float *dists, void **data,
+				  DataType *type, int *numComponents);
 
 	/// get the size of a data type
 	int getDataTypeSize(DataType t);
 
-//////////////////////////////////////////////////////////////////////////
-// The codes below are from reader.c
-//////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////
+	// The codes below are from reader.c
+	//////////////////////////////////////////////////////////////////////////
 
-#include "sysconf.h"
+	#include "sysconf.h"
 
-	void readData(char *filename, int *sizes, float *dists, void **data, 
-		DataType *type, int *numComponents)
+	void readData(char *filename, int *sizes, float *dists, void **data,
+				  DataType *type, int *numComponents)
 	{
 		char *cp, line[100], rawFilename[100];
 		int parseError, dataTypeSize = 0;
 		size_t size;
 		FILE *fp;
 
-		if (! (fp = fopen(filename, "rb"))) {
+		if (! (fp = fopen(filename, "rb")))
+		{
 			perror("opening .dat file failed");
 			exit(1);
 		}
 
 		parseError = 0;
-		while (fgets(line, sizeof(line), fp)) {
-			if (strstr(line, "ObjectFileName")) {
-				if (! (cp = strchr(line, ':'))) {
+		while (fgets(line, sizeof(line), fp))
+		{
+			if (strstr(line, "ObjectFileName"))
+			{
+				if (! (cp = strchr(line, ':')))
+				{
 					parseError = 1;
 					break;
 				}
-				if (sscanf(cp + 1, "%s", rawFilename) != 1)	{
+				if (sscanf(cp + 1, "%s", rawFilename) != 1)
+				{
 					parseError = 1;
 					break;
 				}
-			} else if (strstr(line, "Resolution")) {
-				if (! (cp = strchr(line, ':'))) {
+			}
+			else if (strstr(line, "Resolution"))
+			{
+				if (! (cp = strchr(line, ':')))
+				{
 					parseError = 1;
 					break;
 				}
-				if (sscanf(cp + 1, "%i %i %i", 
-					&sizes[0], &sizes[1], &sizes[2]) != 3) {
-						parseError = 1;
-						break;
-				}
-			} else if (strstr(line, "SliceThickness")) {
-				if (! (cp = strchr(line, ':'))) {
+				if (sscanf(cp + 1, "%i %i %i",
+						   &sizes[0], &sizes[1], &sizes[2]) != 3)
+				{
 					parseError = 1;
 					break;
 				}
-				if (sscanf(cp + 1, "%f %f %f", 
-					&dists[0], &dists[1], &dists[2]) != 3) {
-						parseError = 1;
-						break;
+			}
+			else if (strstr(line, "SliceThickness"))
+			{
+				if (! (cp = strchr(line, ':')))
+				{
+					parseError = 1;
+					break;
 				}
-			} else if (strstr(line, "Format")) {
-				if (strstr(line, "UCHAR")) {
+				if (sscanf(cp + 1, "%f %f %f",
+						   &dists[0], &dists[1], &dists[2]) != 3)
+				{
+					parseError = 1;
+					break;
+				}
+			}
+			else if (strstr(line, "Format"))
+			{
+				if (strstr(line, "UCHAR"))
+				{
 					*type = DATRAW_UCHAR;
 					*numComponents = 1;
 					dataTypeSize = sizeof(char);
-				} else if (strstr(line, "USHORT")) {
+				}
+				else if (strstr(line, "USHORT"))
+				{
 					*type = DATRAW_USHORT;
 					*numComponents = 1;
 					dataTypeSize = sizeof(short);
-				} else if (strstr(line, "FLOAT")) {
+				}
+				else if (strstr(line, "FLOAT"))
+				{
 					char *cp, temp[100];
 
 					*type = DATRAW_FLOAT;
 					dataTypeSize = sizeof(float);
 
-					if (! (cp = strchr(line, ':'))) {
+					if (! (cp = strchr(line, ':')))
+					{
 						parseError = 1;
 						break;
 					}
-					if (sscanf(cp + 1, "%s", temp) != 1) {
+					if (sscanf(cp + 1, "%s", temp) != 1)
+					{
 						parseError = 1;
 						break;
 					}
 
 					cp = temp;
-					while (*cp && ! isdigit(*cp)) {
+					while (*cp && ! isdigit(*cp))
+					{
 						cp++;
 					}
-					if (*cp) {
-						if (sscanf(cp, "%i", numComponents) != 1) {
+					if (*cp)
+					{
+						if (sscanf(cp, "%i", numComponents) != 1)
+						{
 							parseError = 1;
 							break;
 						}
-					} else {
+					}
+					else
+					{
 						*numComponents = 1;
 					}
-				} else {
+				}
+				else
+				{
 					fprintf(stderr, "cannot process data other than of "
-						"UCHAR and FLOAT* format\n");
+							"UCHAR and FLOAT* format\n");
 					exit(1);
 				}
-			} 
-			
-			//////////////////////////////////////////////////////////////////////////		
-			else if (strstr(line, "TaggedFileName")) {
-				if (! (cp = strchr(line, ':'))) {
+			}
+
+			//////////////////////////////////////////////////////////////////////////
+			else if (strstr(line, "TaggedFileName"))
+			{
+				if (! (cp = strchr(line, ':')))
+				{
 					parseError = 1;
 					break;
 				}
-				if (sscanf(cp + 1, "%s", rawFilename) != 1)	{
+				if (sscanf(cp + 1, "%s", rawFilename) != 1)
+				{
 					parseError = 1;
 					break;
 				}
 			}
 			//////////////////////////////////////////////////////////////////////////
-			
-			else {
+
+			else
+			{
 				fprintf(stderr, "skipping line %s", line);
 			}
 		}
 
-		if (parseError) {
+		if (parseError)
+		{
 			fprintf(stderr, "parse error: %s\n", line);
 			exit(1);
 		}
 
 		fclose(fp);
 
-		if (! (fp = fopen(rawFilename, "rb"))) {
+		if (! (fp = fopen(rawFilename, "rb")))
+		{
 			char temp[100];
 			strcpy(temp, filename);
-			if (! (cp = strrchr(temp, DIR_SEP))) { 
+			if (! (cp = strrchr(temp, DIR_SEP)))
+			{
 				perror("opening .raw file failed");
 				exit(1);
 			}
 			strcpy(cp + 1, rawFilename);
-			if (! (fp = fopen(temp, "rb"))) {
+			if (! (fp = fopen(temp, "rb")))
+			{
 				perror("opening .raw file failed");
 				exit(1);
 			}
@@ -162,23 +200,26 @@ namespace volume_utility{
 
 		size = sizes[0] * sizes[1] * sizes[2];
 
-		if (! (*data = malloc(size * *numComponents * dataTypeSize))) {
+		if (! (*data = malloc(size * *numComponents * dataTypeSize)))
+		{
 			fprintf(stderr, "not enough memory for volume data\n");
 			exit(1);
 		}
 
-		if (fread(*data, dataTypeSize, size * *numComponents, fp) != 
-			size * *numComponents) {
-				fprintf(stderr, "reading volume data failed");
-				exit(1);
+		if (fread(*data, dataTypeSize, size * *numComponents, fp) !=
+				size * *numComponents)
+		{
+			fprintf(stderr, "reading volume data failed");
+			exit(1);
 		}
 
-		fclose(fp);	
+		fclose(fp);
 	}
 
 	int getDataTypeSize(DataType t)
 	{
-		switch(t) {
+		switch(t)
+		{
 		case DATRAW_UCHAR:
 			return 1;
 			break;
