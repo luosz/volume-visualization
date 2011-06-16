dfb394a92d9bc9a57cd5b2aa8f67b6c0d95bf0c2
ark <ark@localhost>
2011-06-16 11:31:34 +0800

    change namespaces

--- volume_ray_casting/my_raycasting/VolumeReader.h
+++ volume_ray_casting/my_raycasting/VolumeReader.h
@@ -155,8 +155,8 @@ namespace volume_utility
 			int sizes[3];
 			int color_omponent_number;
 			float dists[3];
-			DataType type;
-			volume_utility::readData(filename, sizes, dists, data_ptr, &type, &color_omponent_number);
+			file_utility::DataType type;
+			file_utility::readData(filename, sizes, dists, data_ptr, &type, &color_omponent_number);
 
 			// Please set the properties of class volume to protected, in order to set their values.
 			length = sizes[0];
@@ -166,13 +166,13 @@ namespace volume_utility
 
 			switch (type)
 			{
-			case DATRAW_UCHAR:
+			case file_utility::DATRAW_UCHAR:
 				strcpy(format, "UCHAR");
 				dataTypeSize = sizeof(unsigned char);
 				range = 256;
 				accessor = new AccessGeneric<unsigned char *>(data);
 				break;
-			case DATRAW_USHORT:
+			case file_utility::DATRAW_USHORT:
 				strcpy(format, "USHORT");
 				dataTypeSize = sizeof(unsigned short);
 				range = 65536;
--- volume_ray_casting/my_raycasting/textfile.h
+++ volume_ray_casting/my_raycasting/textfile.h
@@ -6,5 +6,87 @@
 // or explicit are given
 //////////////////////////////////////////////////////////////////////
 
-char *textFileRead(char *fn);
-int textFileWrite(char *fn, char *s);
+namespace file_utility
+{
+	/// read text from text file
+	char *textFileRead(char *fn);
+
+	/// write text to text file
+	int textFileWrite(char *fn, char *s);
+
+	//////////////////////////////////////////////////////////////////////////
+	// The codes below are from textfile.cpp
+	//////////////////////////////////////////////////////////////////////////
+
+	// textfile.cpp
+	//
+	// simple reading and writing for text files
+	//
+	// www.lighthouse3d.com
+	//
+	// You may use these functions freely.
+	// they are provided as is, and no warranties, either implicit,
+	// or explicit are given
+	//////////////////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+	/// read text files
+	char *textFileRead(char *fn)
+	{
+
+
+		FILE *fp;
+		char *content = NULL;
+
+		int count=0;
+
+		if (fn != NULL)
+		{
+			fp = fopen(fn,"rt");
+
+			if (fp != NULL)
+			{
+
+				fseek(fp, 0, SEEK_END);
+				count = ftell(fp);
+				rewind(fp);
+
+				if (count > 0)
+				{
+					content = (char *)malloc(sizeof(char) * (count+1));
+					count = fread(content,sizeof(char),count,fp);
+					content[count] = '\0';
+				}
+				fclose(fp);
+			}
+		}
+		return content;
+	}
+
+	/// write text files
+	int textFileWrite(char *fn, char *s)
+	{
+
+		FILE *fp;
+		int status = 0;
+
+		if (fn != NULL)
+		{
+			fp = fopen(fn,"w");
+
+			if (fp != NULL)
+			{
+
+				if (fwrite(s,sizeof(char),strlen(s),fp) == strlen(s))
+					status = 1;
+				fclose(fp);
+			}
+		}
+		return(status);
+	}
+
+}
