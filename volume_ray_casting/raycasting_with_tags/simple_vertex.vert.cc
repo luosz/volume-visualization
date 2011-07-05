5c7144ba04a6ed308c2315438f1157ddbf878e2d
ark <ark@localhost>
2011-07-05 18:12:31 +0800

    add lighting

--- volume_ray_casting/raycasting_with_tags/simple_vertex.vert.cc
+++ volume_ray_casting/raycasting_with_tags/simple_vertex.vert.cc
@@ -1,11 +1,28 @@
 /**
  * Simply pass through the provided vertex position.
  */
+// for raycasting
 varying vec4 position;
+
+// for lighting
+uniform vec3 fvLightPosition;
+uniform vec3 fvEyePosition;
+
+varying vec3 ViewDirection;
+varying vec3 LightDirection;
+varying vec3 Normal;
 
 void main() {
     gl_TexCoord[0] = gl_MultiTexCoord0;
 	gl_TexCoord[1] = gl_MultiTexCoord1;
-	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+	//gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+	gl_Position = ftransform();
 	position = gl_Position;
+
+	// for lighting
+	vec4 fvObjectPosition = gl_ModelViewMatrix * gl_Vertex;
+
+	ViewDirection  = fvEyePosition - fvObjectPosition.xyz;
+	LightDirection = fvLightPosition - fvObjectPosition.xyz;
+	Normal         = gl_NormalMatrix * gl_Normal;
 }
