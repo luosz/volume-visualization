bb257956ca8eec69c25675fb2d0048fc2719fe1c
ark <ark@localhost>
2011-07-07 15:32:59 +0800

    there are unknown errors

--- volume_ray_casting/raycasting_with_tags/simple_vertex.vert.cc
+++ volume_ray_casting/raycasting_with_tags/simple_vertex.vert.cc
@@ -1,6 +1,6 @@
 /**
- * Simply pass through the provided vertex position.
- */
+* Simply pass through the provided vertex position.
+*/
 
 // for raycasting
 varying vec4 position;
@@ -13,17 +13,22 @@ varying vec3 ViewDirection;
 varying vec3 LightDirection;
 //varying vec3 Normal;
 
+varying vec4 fvObjectPosition;
+
 void main() {
-    gl_TexCoord[0] = gl_MultiTexCoord0;
+	gl_TexCoord[0] = gl_MultiTexCoord0;
 	gl_TexCoord[1] = gl_MultiTexCoord1;
 	//gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 	gl_Position = ftransform();
 	position = gl_Position;
 
+	//Texcoord    = gl_MultiTexCoord0.xy;
+
 	// for lighting
 	vec4 fvObjectPosition = gl_ModelViewMatrix * gl_Vertex;
-
 	ViewDirection  = fvEyePosition - fvObjectPosition.xyz;
 	LightDirection = fvLightPosition - fvObjectPosition.xyz;
 	//Normal         = gl_NormalMatrix * gl_Normal;
+
+	//fvObjectPosition = gl_ModelViewMatrix * gl_Vertex;
 }
